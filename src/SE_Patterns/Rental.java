a9e86f666a8a9998bf9223837d822e02ab1e32d2
Patrick <Patrick@Ronny-PC>
2014-05-22 18:15:19 +0200

    Extract Method

--- src/SE_Patterns/Rental.java
+++ src/SE_Patterns/Rental.java
@@ -1,35 +1,48 @@
 package SE_Patterns;
+
 class Rental {
-    private Movie movie;
-    private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
-    }
-    public int getDaysRented() {
-        return daysRented;
-    }
-    public Movie getMovie() {
-        return movie;
-    }
-    
+	private Movie movie;
+	private int daysRented;
+
+	public Rental(Movie newmovie, int newdaysRented) {
+		movie = newmovie;
+		daysRented = newdaysRented;
+	}
+
+	public int getDaysRented() {
+		return daysRented;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
 	public double getCharge() {
 		double result = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-            	result += 2;
-            	if (getDaysRented() > 2)
-                	result += (getDaysRented() - 2) * 1.5;
-            	break;
-            case Movie.NEW_RELEASE:
-            	result += getDaysRented() * 3;
-            	break;
-            case Movie.CHILDRENS:
-            	result += 1.5;
-            	if (getDaysRented() > 3)
-                	result += (getDaysRented() - 3) * 1.5;
-            	break;
-        }
-        return result;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2)
+				result += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (getDaysRented() > 3)
+				result += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return result;
 	}
+
+	public int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
+
 }
