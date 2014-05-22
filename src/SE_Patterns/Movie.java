9c164cab141643a912cfd1eea2d57cce7b2d5e82
Patrick <Patrick@Ronny-PC>
2014-05-22 19:01:17 +0200

    Replace Type Code with State/Strategy

--- src/SE_Patterns/Movie.java
+++ src/SE_Patterns/Movie.java
@@ -4,44 +4,41 @@ public class Movie {
 	public static final int CHILDRENS = 2;
 	public static final int REGULAR = 0;
 	public static final int NEW_RELEASE = 1;
-	private String title;
-	private int priceCode;
+	private String _name;
+	private int _priceCode;
+	private Price _price;
 
-	public Movie(String newtitle, int newpriceCode) {
-		title = newtitle;
-		priceCode = newpriceCode;
+	public Movie(String name, int priceCode) {
+		_name = name;
+		setPriceCode(priceCode);
 	}
 
 	public int getPriceCode() {
-		return priceCode;
+		return _price.getPriceCode();
 	}
 
 	public void setPriceCode(int arg) {
-		priceCode = arg;
+		switch (arg) {
+		case REGULAR:
+			_price = new RegularPrice();
+			break;
+		case CHILDRENS:
+			_price = new ChildrensPrice();
+			break;
+		case NEW_RELEASE:
+			_price = new NewReleasePrice();
+			break;
+		default:
+			throw new IllegalArgumentException("Incorrect Price Code");
+		}
 	}
 
 	public String getTitle() {
-		return title;
+		return _name;
 	}
 
 	double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
-			break;
-		}
-		return result;
+		return _price.getCharge(daysRented);
 	}
 
 	public int getFrequentRenterPoints(int daysRented) {
