178847d44f966ad1ec892dd72aaa064774d97c03
Patrick <Patrick@Ronny-PC>
2014-05-22 19:13:45 +0200

    Replace Conditional with Polymorphism

--- src/SE_Patterns/Price.java
+++ src/SE_Patterns/Price.java
@@ -1,42 +1,51 @@
 package SE_Patterns;
 
 abstract class Price {
+	abstract double getCharge(int daysRented);
+
 	abstract int getPriceCode();
 
-	public double getCharge(int daysRented) {
-		double result = 0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented > 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented - 3) * 1.5;
-			break;
-		}
-		return result;
+	int getFrequentRenterPoints(int daysRented) {
+		return 1;
 	}
 }
 
 class ChildrensPrice extends Price {
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3)
+			result += (daysRented - 3) * 1.5;
+		return result;
+	}
+
+	@Override
 	int getPriceCode() {
 		return Movie.CHILDRENS;
 	}
 }
 
 class NewReleasePrice extends Price {
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
+
+	int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2 : 1;
+	}
+	
 	int getPriceCode() {
 		return Movie.NEW_RELEASE;
 	}
 }
 
 class RegularPrice extends Price {
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2)
+			result += (daysRented - 2) * 1.5;
+		return result;
+	}
+
 	int getPriceCode() {
 		return Movie.REGULAR;
 	}
